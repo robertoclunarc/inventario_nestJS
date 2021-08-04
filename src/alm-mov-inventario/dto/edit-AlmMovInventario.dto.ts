18e5c492b6eabf59ba97d074a10a2fa99ee824ec
Roberto Lunar <desarrollo2.acero@gmail.com>
2021-08-04 14:26:27 -0400

    ajustes en el edit-almmovinventarioDTO

--- src/alm-mov-inventario/dto/edit-AlmMovInventario.dto.ts
+++ src/alm-mov-inventario/dto/edit-AlmMovInventario.dto.ts
@@ -1,5 +1,4 @@
 import { CreateAlmMovInventarioDTO } from './create-AlmMovInventario.dto';
-
 import { PartialType } from '@nestjs/mapped-types';
 
 export class EditAlmMovInventarioDTO extends PartialType(CreateAlmMovInventarioDTO ) {} 
