5a79332fd9f18befb161dab3159496a4b52d9606
Roberto Lunar <desarrollo2.acero@gmail.com>
2021-08-20 13:38:46 -0400

    ajuste en el config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default  () => ({
-    MYSQL_SERVER: process.env.MYSQL_SERVER || 'localhost',
-    MYSQL_USER: process.env.MYSQL_USER || 'root',    
-    MYSQL_DB: process.env.MYSQL_DB || 'intranet',      
-    MYSQL_PW: process.env.MYSQL_PW || '.4C3r04dm1n',
-    MYSQL_PORT: parseInt(process.env.MYSQL_PORT) || 3306,
+    MYSQL_SERVER: process.env.MYSQL_SERVER,
+    MYSQL_USER: process.env.MYSQL_USER,    
+    MYSQL_DB: process.env.MYSQL_DB,      
+    MYSQL_PW: process.env.MYSQL_PW,
+    MYSQL_PORT: parseInt(process.env.MYSQL_PORT),
 })
