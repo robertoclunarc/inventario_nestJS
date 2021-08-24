5c9ae66bee07984f7fdeaa3a39a10851353160b3
Roberto Lunar <desarrollo2.acero@gmail.com>
2021-08-24 12:38:52 -0400

    update en variables de entorno

--- app/src/config/configuration.ts
+++ app/src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default  () => ({
-    MYSQL_SERVER: process.env.DESA_MYSQL_SERVER,
-    MYSQL_USER: process.env.DESA_MYSQL_USER,    
-    MYSQL_DB: process.env.DESA_MYSQL_DB,      
-    MYSQL_PW: process.env.DESA_MYSQL_PASS,
-    MYSQL_PORT: parseInt(process.env.DESA_MYSQL_PORT),
+    MYSQL_SERVER: process.env.MYSQL_SERVER,
+    MYSQL_USER: process.env.MYSQL_USER,    
+    MYSQL_DB: process.env.MYSQL_DB,      
+    MYSQL_PW: process.env.MYSQL_PASS,
+    MYSQL_PORT: parseInt(process.env.MYSQL_PORT),
 })
